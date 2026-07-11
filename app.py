4aace3f5d83aa8cca6c0ea051f4dd94251cc98e7
rafaelmurgi <rafaelmurgi@gmail.com>
2026-07-11 20:53:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,13 +9,18 @@ st.set_page_config(
     layout="wide"
 )
 
-<script type="text/javascript">
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "xl0p64s7sr");
-</script>
+components.html(
+    """
+    <script type="text/javascript">
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "xl0p64s7sr");
+    </script>
+    """,
+    height=0,
+)
 
 st.markdown(
     """
