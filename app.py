15dc128ee1fb47d246cec1a576a955857565a022
rafaelmurgi <rafaelmurgi@gmail.com>
2026-07-11 20:49:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import streamlit.components.v1 as components
 import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
@@ -7,6 +8,14 @@ st.set_page_config(
     page_title="Brazil Imports Intelligence Dashboard",
     layout="wide"
 )
+
+<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "xl0p64s7sr");
+</script>
 
 st.markdown(
     """
